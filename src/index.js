5257ca0bb283f0edc5c1b9df0ed1ca08707a29fb
Ashutosh Kumar Singh <ashu2016@gmail.com>
2018-05-24 08:35:44 +0530

    Lecture 23: Computed properties
    use Vue computed to defined computed properties. Definition is same as a function but they are used like a normal property.
    
    Definition : 
    computed : {
        output: function() {
          alert("Output");
          return this.counter > 5 ? 'greater than 5' : 'smaller than 5';
          
        }
      }
    
    Usage : 
    <p>Output : {{output}}</p>.
    
    They are executed only when the source property gets changed.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,20 @@ new Vue({
   el: "#app",
   data: {
     counter: 0,
-    result : ""
+    secondCounter:0
+   
   },
   methods: {
-    increment : function(){
+    result : function(){
+      alert("result");
+      return  this.counter > 5 ? 'greater than 5' : 'smaller than 5';
+    }
+  },
+  computed : {
+    output: function() {
+      alert("Output");
+      return this.counter > 5 ? 'greater than 5' : 'smaller than 5';
       
-      this.counter++;
-      this.result =  this.counter > 5 ? 'greater than 5' : 'smaller than 5';
     }
   }
 });
