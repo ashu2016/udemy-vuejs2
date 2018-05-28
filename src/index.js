4345a02a5314d6836888779ba0605f34439947dc
Ashutosh Kumar Singh <ashu2016@gmail.com>
2018-05-28 07:40:37 +0530

    lecture 26: dynamic styling with css
    you can use :class to dynamically assign a class on the go to any element. 
    
    It takes am object like a css  class where a variable is assigned a value. 
    
    :class = "{'red':attachRed}"

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 new Vue({
   el: "#app",
   data: {
-    counter: 0,
-    counter1 :0    
-  }, 
-  computed : {
-    result: function() {      
-      return this.counter < 37 ? 'Not there yet' : 'Done';
-    }
-  },
-  watch :{
-    result: function(value){
-      var vm = this;
-      setTimeout(function(){
-          vm.result = 'Not there yet';
-          vm.counter =  0;
-      },5000);
-    }
+    attachRed: false
+  
   }
 });
